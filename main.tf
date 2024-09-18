8ee4f5c0dbb6180e8a30380335e3e1a002ee6f41
justin-champtitles <justin.reeves@champtitles.com>
2024-09-18 09:55:51 -0500

    Add archive_file

--- main.tf
+++ main.tf
@@ -3,3 +3,10 @@ data "aws_region" "this" {}
 resource "random_id" "this" {
   byte_length = 3
 }
+
+data "archive_file" "this" {
+  type             = "zip"
+  output_file_mode = "0666"
+  source_dir      = "${path.module}/src"
+  output_path      = "${path.module}/package.zip"
+}
