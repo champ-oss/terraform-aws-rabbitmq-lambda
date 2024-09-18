8e7989865b5b96230cfa5255c183d080225c4e51
justin-champtitles <justin.reeves@champtitles.com>
2024-09-18 12:54:44 -0500

    Implement logic to get SSM parameter value

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-import os
 import logging
-import pika
+import os
 import ssl
+
 import boto3
+
+import pika
+from pika.channel import Channel
 
 logging.basicConfig(level=logging.INFO)
 
@@ -12,37 +15,42 @@ USER: str = os.environ.get('RABBITMQ_USER', 'guest')
 PASSWORD: str = os.environ.get('RABBITMQ_PASSWORD', 'guest')
 PASSWORD_SSM: str = os.environ.get('RABBITMQ_PASSWORD_SSM')
 SSL_OPTIONS = pika.SSLOptions(context=ssl.SSLContext(ssl.PROTOCOL_TLSv1_2))
-PARAMS = pika.ConnectionParameters(
-    host=HOST,
-    port=PORT,
-    credentials=pika.PlainCredentials(USER, PASSWORD),
-    ssl_options=SSL_OPTIONS if PORT == 5671 else None
-)
 AWS_REGION = os.environ.get('AWS_REGION', 'us-east-1')
 SSM_CLIENT = boto3.client('ssm', region_name=AWS_REGION)
 
 
 def handler(event: dict, __) -> str:
-    # if PASSWORD_SSM:
-    #     response = SSM_CLIENT.get_parameter(Name=PASSWORD_SSM,WithDecryption=True)
-    #     password = response.get('Parameter', {}).get('Value')
-
     exchange = event.get('exchange', '')
     routing_key = event.get('routing_key')
     body = event.get('body')
     if not routing_key or not body:
         return 'body and routing_key is required'
 
-    logging.info('Connecting to RabbitMQ host: %s:%s', HOST, PORT)
-    connection = pika.BlockingConnection(PARAMS)
-    channel = connection.channel()
-
     logging.info('publishing message: exchange:"%s" routing key:"%s"', exchange, routing_key)
+    channel = _get_channel()
     channel.basic_publish(
         exchange=exchange,
         routing_key=routing_key,
-        body=body
-    )
+        body=body)
+
+
+def _get_channel() -> Channel:
+    logging.info('Connecting to RabbitMQ host: %s:%s', HOST, PORT)
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host=HOST,
+        port=PORT,
+        credentials=pika.PlainCredentials(USER, _get_password()),
+        ssl_options=SSL_OPTIONS if PORT == 5671 else None
+    ))
+    return connection.channel()
+
+
+def _get_password() -> str:
+    if not PASSWORD_SSM:
+        return PASSWORD
+    logging.info('loading password from SSM parameter:%s', PASSWORD_SSM)
+    response = SSM_CLIENT.get_parameter(Name=PASSWORD_SSM, WithDecryption=True)
+    return response.get('Parameter', {}).get('Value')
 
 
 if __name__ == '__main__':
